ff2fb5d05f25b9a37b354ea9b8a94daec0d2b8c3
idevyatkov <idevyatkov@callinsight.ru>
2020-12-31 13:30:21 +0500

    Добавил свзи между таблицами

--- src/main/java/com/example/sweater/controller/RegistrationController.java
+++ src/main/java/com/example/sweater/controller/RegistrationController.java
@@ -31,7 +31,7 @@ public class RegistrationController {
         user.setActive(true);
         user.setRoles(Collections.singleton(Role.USER));
         userRepository.save(user);
-        return "redirect:/main";
+        return "redirect:/login";
     }
 
 }
